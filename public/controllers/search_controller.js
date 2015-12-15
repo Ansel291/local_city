6a7283eedb20ddde3a97494ff98967798b3eecb3
Jason Asahara <jasahara@yahoo.com>
2015-12-15 11:56:21 -0800

    phil's big overhaul help

--- public/controllers/search_controller.js
+++ public/controllers/search_controller.js
@@ -5,10 +5,29 @@
     .module("localCityApp")
     .controller("SearchController", SearchController);
 
-  SearchController.$inject = [];
+  SearchController.$inject = ["$log", "$http"];
 
-  function SearchController() {
+  function SearchController($log, $http) {
     var vm = this;
+
+    vm.city  = "";
+    vm.query = "";
+
+    vm.search = function() {
+      var queryUri = "http://localhost:3000/api/venues?near=" + vm.city + "&query=" + vm.query;
+
+      $log.log("Sending request for", vm.query, "in", vm.city, "…");
+      $log.log(queryUri);
+
+      $http({
+        method: "GET",
+        url:    queryUri
+      }).then(function(response) {
+        $log.log(response.data.venues);
+      }).catch(function(err) {
+        $log.log(err);
+      });
+    };
 
   }
 
